7babc82e9c1f7ff0d78a62492c164068dc170c36
arrudagates <arrudagates@users.noreply.github.com>
2021-10-11 14:15:09 +0000

    deploy: 9956c084c8a63c2955f3f061acbae8249e33babc

--- index-14d87a79a5eb4517.js
+++ index-14d87a79a5eb4517.js
@@ -267,7 +267,7 @@ async function load(module, imports) {
 
 async function init(input) {
     if (typeof input === 'undefined') {
-        input = new URL('index-4079719f4d1e79ea_bg.wasm', import.meta.url);
+        input = new URL('index-14d87a79a5eb4517_bg.wasm', import.meta.url);
     }
     const imports = {};
     imports.wbg = {};
@@ -539,12 +539,12 @@ async function init(input) {
     imports.wbg.__wbindgen_throw = function(arg0, arg1) {
         throw new Error(getStringFromWasm0(arg0, arg1));
     };
-    imports.wbg.__wbindgen_closure_wrapper85 = function(arg0, arg1, arg2) {
-        var ret = makeMutClosure(arg0, arg1, 14, __wbg_adapter_18);
+    imports.wbg.__wbindgen_closure_wrapper86 = function(arg0, arg1, arg2) {
+        var ret = makeMutClosure(arg0, arg1, 15, __wbg_adapter_18);
         return addHeapObject(ret);
     };
-    imports.wbg.__wbindgen_closure_wrapper266 = function(arg0, arg1, arg2) {
-        var ret = makeMutClosure(arg0, arg1, 95, __wbg_adapter_21);
+    imports.wbg.__wbindgen_closure_wrapper271 = function(arg0, arg1, arg2) {
+        var ret = makeMutClosure(arg0, arg1, 98, __wbg_adapter_21);
         return addHeapObject(ret);
     };
 
